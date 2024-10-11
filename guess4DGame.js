91493a9efa883d0508de28c1ed3f8a8614f93a64
kfhwang <kfhwang@nutc.edu.tw>
2024-10-11 16:01:04 +0800

    20241011

--- guess4DGame.js
+++ guess4DGame.js
@@ -9,16 +9,45 @@ for(var i=0;i<4;i++){
   Ans[i] = Ans[rand];
   Ans[rand] = temp;
 }
-
+Ans = Ans.slice(0,4);
+var gCount=10;
 do{
-   var G=readline.questionInt("Please input 4 digits? ");
-}while(G<1000 || G>=10000);
-var Gstr = G.toString();
-//adjuge ?A?B
-var countA=0,countB=0;
-for(){//Ans[0]~A[3]
-    for(){//Gstr[0]!Gstr[3]
+  do{
+    var G=readline.questionInt("Please input 4 digits? ");
+  }while(String(G).length!=4)
+  //}while(G<1000 || G>=10000);
+  var Gstr = G.toString();
+  //adjuge ?A?B
+  var countA=0,countB=0;
+  for(var i=0;i<4;i++){//Ans[0]~A[3]
+      if(Ans[i] == Gstr[i])
+        countA++;
+      else if(Ans.includes(parseInt(Gstr[i]))){
+        countB++;
+      }
+      
+      // for(var j=0;j<4;j++){//Gstr[0]!Gstr[3]
+      //     if(Ans[i]==Gstr[j]){
+      //       if(i==j)
+      //         countA++;
+      //       else 
+      //         countB++;
+      //     }
+      // }
 
-    }
-}
-//Win?
+      // if(Ans[i] == Gstr[i])
+      //   countA++;
+      // else if(Gstr.indexOf(Ans[i]>=0)){
+      //   countB++;
+      // }
+  }
+  gCount--;
+  console.log("Result: "+countA+"A"+countB+"B");//"${CountA}A${countB}B"
+  //Win?
+  if(countA==4){
+    console.log("You win!");
+  }
+}while(countA<4 && gCount>0)
+
+if(countA<4)
+  console.log("You lose!");
